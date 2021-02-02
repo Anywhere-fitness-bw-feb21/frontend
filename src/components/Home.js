6931013718c2abbf5512e8503992495f799b175c
Jakobi Matagi <jkmatagi@hotmail.com>
2021-02-02 14:29:54 -0700

    built a basic component for a landing page and routed it to /

---
+++ src/components/Home.js
@@ -0,0 +1,23 @@
+import react from 'react';
+
+
+
+
+ function LandingPage(){
+    return(
+        <div>
+        <h1>Anywhere Fitness</h1>
+        <h2>Focus | Desire | Discipline</h2>
+
+        <button>Login</button>
+        <button>Register</button>
+        </div>
+    )
+
+
+
+}
+
+
+
+export default LandingPage;
