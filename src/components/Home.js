89bb87b81e82b00c1a59d4c0ba10688f76eb61ac
Fidan Novruzova <yf@Fidans-MacBook-Pro.local>
2021-02-02 16:24:45 -0800

    media

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,10 +6,10 @@ import {Link} from 'react-router-dom'
  function Home(){
     return(
         <StyledDivHome>
-        <div>
+       
         <h1>Anywhere Fitness</h1>
         <h2>Focus | Desire | Discipline</h2>
-
+        <div>
         <Link to="/Login"><button>Login</button></Link>
         <Link to="/Register"><button >Register</button></Link>
         </div>
--- src/styledComponents/StyledDivHome.js
+++ src/styledComponents/StyledDivHome.js
@@ -10,7 +10,7 @@ align-items: center;
 width: 60%;
 margin: 0 auto;
 padding: 1.5rem 0;
-
+text-align: center;
 
 h1{
 font-size: 3rem;
@@ -18,12 +18,37 @@ font-size: 3rem;
 h2{
 align-items: center;
 justify-content:center;
-margin-left: 3rem;
+/* margin-left: 3rem; */
+}
+div{
+    width: 40%;
+    display: flex;
+    justify-content: space-around;
 }
 button{
-margin-left: 5rem;
+/* margin-left: 5rem; */
+padding: 0.5rem 0;
+width: 70px;
 justify-content:center;
 align-items: center;
 }
+@media(max-width: 750px){
+    width: 100%;
+   
+}
+@media ( max-width: 400px){
+    h1{
+        font-size: 2rem;
+    }
+    h2{
+        font-size: 1.2rem;
+    }
 
+    div{
+        height: 30%;
+        flex-direction: column;
+        align-items: center;
+    }
+    
+}
 `
