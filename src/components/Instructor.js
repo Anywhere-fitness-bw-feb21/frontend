c0ff2b770afc99bdf5fbd1c025c5acd028ee11f5
fidan93 <72060780+fidan93@users.noreply.github.com>
2021-08-04 00:20:42 -0400

    Update Instructor.js

--- src/components/Instructor.js
+++ src/components/Instructor.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { useHistory } from "react-router-dom";
 import { connect } from "react-redux";
 import { getClasses, setEditing } from "../actions/index";
 import styled from "styled-components";
